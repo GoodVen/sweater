42ec05c962d6b06baf7a3fbb0adbeadcd6b9c388
Sergey <45140921+GoodVen@users.noreply.github.com>
2022-01-18 18:06:59 +0100

    Initial commit

---
+++ src/main/java/com/example/sweater/Application.java
@@ -0,0 +1,17 @@
+package com.example.sweater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//запуск всего приложения и подключение Spring мощностей
+
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/sweater/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.example.sweater.repos;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.sweater.domain.Message;
+
+import java.util.List;
+
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+}
